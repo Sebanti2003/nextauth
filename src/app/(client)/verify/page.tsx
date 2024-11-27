380c0c750773219e514295d8cb5577b61e5cba7a
Sebanti2003 <dasguptasebanti2003@gmail.com>
2024-11-27 22:56:09 +0530

    done nothing much just a change in the verify route in the client side

--- src/app/(client)/verify/page.tsx
+++ src/app/(client)/verify/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useSearchParams } from "next/navigation";
 const page = () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     const searchparams= useSearchParams();
     const token=searchparams.get("token");
   return <div>verify {token}</div>;
